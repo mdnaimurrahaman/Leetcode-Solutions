fe64e6d796ecfd219a82abe90946b6819a6ad814
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-19 23:38:04 +0600

    Palindrome Number Leedcode problem  solution

--- Easy.js
+++ Easy.js
@@ -195,7 +195,7 @@ console.log(migratroyBirds(testarry))
 /*
 Exercise 8
 ----------
-Two Sum
+Two Sum || LeedCode problem
 */
 /*
 //Solution:
@@ -208,4 +208,23 @@ Two Sum
         }
     }
 }
+*/
+/*
+Exercise 9
+----------
+Palindrome Number || Leedcode  problem
+*/
+/*
+//Solution:
+};
+var isPalindrome = function(x) {
+    if (x<0 || x%10 === 0 && x !== 0) return false;
+    let reverse = 0;
+    let num = x;
+    while(num > reverse) {
+        reverse = num%10 + reverse*10;
+        num = parseInt(num/10);
+    }
+    return  (num === reverse || num === parseInt(reverse/10));
+};
 */
