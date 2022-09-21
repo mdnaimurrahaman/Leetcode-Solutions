af68c937a185aa47b7ff34076e1c733424b55b11
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-21 23:51:14 +0600

    Valid Parentheses problem solution

--- Easy.js
+++ Easy.js
@@ -298,3 +298,27 @@ let res = longestCommonPrefix(["flower","flow","flight"]);
 
 console.log(res);
 */
+/*
+Exercise 12
+----------
+const isValid = (s) => {
+  const stack = [];
+  
+  for (let i = 0; i < s.length; i += 1) {
+    const top = stack[stack.length - 1];
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      stack.push(s[i]);
+    } else if (s[i] === ')' && top === '(' && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === ']' && top === '[' && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === '}' && top === '{' && stack.length !== 0) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  
+  return stack.length === 0;
+};
+*/
