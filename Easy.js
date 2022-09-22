312e34f4efc39085f829088e951aa7b7bd95382d
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-22 23:47:14 +0600

    Longest common Prefix problem solution

--- Easy.js
+++ Easy.js
@@ -274,3 +274,27 @@ for (let i = 0; i < s.length; i++) {
   return accumulator;
 }
 */
+
+/*
+Exercise 11
+----------
+Longest Common Prefix || Leedcode  problem
+*/
+/*
+//Solution:
+
+var longestCommonPrefix = function(strs) {
+    let prefix = strs.reduce((acc, str) => str.length < acc.length ? str : acc);
+    
+    for (let str of strs) {
+        while (str.slice(0, prefix.length) != prefix) {
+            prefix = prefix.slice(0, -1);
+        }
+    }
+    return prefix;
+};
+
+let res = longestCommonPrefix(["flower","flow","flight"]);
+
+console.log(res);
+*/
