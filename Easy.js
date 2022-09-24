9dae6732b73cb7ee1b8ca00fd03ee9ee0995fa4c
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-24 19:58:58 +0600

    Merge Two Sorted Lists recursion solution

--- Easy.js
+++ Easy.js
@@ -322,3 +322,64 @@ const isValid = (s) => {
   return stack.length === 0;
 };
 */
+
+/*
+Exercise 13
+----------
+Merge Two Sorted Lists  \\ leedCode problem
+*/
+/*
+//Solution #2: Recursion
+var mergeTwoLists = function (l1, l2) {
+    // If either list is empty, return the other list's node
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
+
+    // If l1 is lower
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+    // If l2 is lower
+    else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+};
+*/
+/*
+Exercise 
+----------
+
+*/
+/*
+//Solution
+
+*/
+/*
+Exercise 
+----------
+
+*/
+/*
+//Solution
+
+*/
+/*
+Exercise 
+----------
+
+*/
+/*
+//Solution
+
+*/
+/*
+Exercise 
+----------
+
+*/
+/*
+//Solution
+
+*/
